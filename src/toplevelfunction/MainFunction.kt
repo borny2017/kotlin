084572032ba72997c2d41b7bc84ddf4b9ad51c11
borny <haerta@gmx.de>
2019-09-15 14:19:35 +0200

    test

---
+++ src/toplevelfunction/MainFunction.kt
@@ -0,0 +1,6 @@
+package toplevelfunction
+
+fun main(args: Array<String>) {
+    println("Hello World")
+    testFunction()
+}
---
+++ src/toplevelfunction/Toplevelfunction.kt
@@ -0,0 +1,5 @@
+package toplevelfunction
+
+fun testFunction(){
+    println("This is a top level function")
+}
